d3947054fd541674010579426526578a816beb4f
Cathal Sweeney <cathalsweeney96@hotmail.com>
2024-10-31 15:58:01 -0500

    add a bunch of comments and clean things up

--- other_motor_control/other_motor_control.ino
+++ other_motor_control/other_motor_control.ino
@@ -1,42 +1,56 @@
 #define ENCODER_USE_INTERRUPTS
 
+// Need custom version of Encoder library https://forum.arduino.cc/t/uno-r4-wifi-error-in-encoder-library/1215256/10
 #include <Encoder.h>
 
-#include <PID_v1.h>
 
-// Motor driver pins
+// ---- All the parameters a user might want to change are in this block ---- 
+bool debug = false;
+double setpoint = -56000;  // Desired position
+int wait_time_base = 1; // Wait time in ms before we try update the motor
+int wait_time_flip = 100; // Wait time in ms before we try update the motor after overshooting
+int wait_time_print = 1000; // Wait time in ms before printing again
+int wait_time_home = 1000; // Once we reach our destination, how long do we wait before resetting and moving again
+double base_speed = 30.; // default motor speed
+double floor_speed = 2.; // minimum speed of motor
+int home_limit = 500; // if we have been at the destination for this many checks, we are done
+// --------------------------------------------------------------------------
+
+int wait_time = wait_time_base;
+double speed = base_speed; // FYI technically this gets cast to an int when we write to a pwm pin
+double currentPosition = 0;  // Encoder position
+double last_pos = 0; // What was the encoder position last time we checked
+bool forward = true; // motor direction. true = forward, false = backward
+bool flip = false; // if the motor has just overshot, this will be true for 100ms
+
+int nHome = 0; // number of consecutive times we have checked and the motor is at the destination
+int nSame = 0; // number of consecutive times we have checked and the motor is at the same location (not destination)
+int nRun = 0; // how many times have we reset and started again
+int counter = 0; // number of times loop() has executed. just for debugging
+
+
+// Which pin you use for what matters, don't change without doing some research
+
 const int motorPin1 = 8;
 const int motorPin2 = 10;
-const int pwmPin = 9; // PWM pin for speed control
-// Encoder pins
+const int pwmPin = 9; 
 const int encoderPinA = 3;
 const int encoderPinB = 4;
 
-
-
 //const int motorPin1 = 13;
 //const int motorPin2 = 12;
-//const int pwmPin = 11; // PWM pin for speed control
-//// Encoder pins
+//const int pwmPin = 11; 
 //const int encoderPinA = 5;
 //const int encoderPinB = 6;
 
 
-
-
-// Setpoint and PID parameters
-double setpoint = -56000;  // Desired position
-double currentPosition = 0;  // Encoder position
-double last_pos = 0;
-
-bool forward = true;
+// Initialise all these times to current time as placeholder
+unsigned long last_time = millis(); // timestamp of time we most recently checked on the motor
+unsigned long last_time_home = millis(); // timestamp when we reached our destination ("home") 
+unsigned long last_time_print = millis(); // timestamp when we last printed out values. Warning - printing degrades encoder performance, only do it for debugging	
 
 // Encoder object
-Encoder myEncoder(encoderPinA, encoderPinB);
-
-unsigned long last_time = millis();
-unsigned long last_time_home = millis();
-unsigned long last_time_print = millis();	
+Encoder myEncoder(encoderPinA, encoderPinB); // This guy will keep track of the motor position
 
 void setup() {
   // Initialize motor pins
@@ -50,21 +64,9 @@ void setup() {
 
   last_pos = currentPosition;
 
-  Serial.begin(9600);  // Debugging
-}
+  Serial.begin(9600);  // for printouts
 
-int wait_time = 1;
-int wait_time_print = 1000;
-int wait_time_home = 1000;
-double base_speed = 30.;
-double speed = base_speed;
-double last_output;
-int nHome = 0;
-int nSame = 0;
-bool flip = false;
-int counter = 0;
-double floor_speed = 2;
-int nRun = 0;
+}
 
 void loop() {
   // Read the current encoder position
@@ -73,21 +75,21 @@ void loop() {
   unsigned long time = millis();
   counter++;
 
-//  if( (time - last_time_print) > wait_time_print){
-//    last_time_print = time;
-//    print();
-//  }  
+  if( debug && (time - last_time_print) > wait_time_print){
+    last_time_print = time;
+    print();
+  }  
 
-  if(nHome < 500){
-    if( (time - last_time > wait_time)  ){
+  if(nHome < home_limit){
+    if( (time - last_time > wait_time)  ){ 
       last_time = time;
-  
+      
       if(flip){
-        wait_time = 1;
+        wait_time = wait_time_base; // go back to default wait_time
       }
 
-      if(currentPosition == last_pos){
-        nSame++;
+      if(currentPosition == last_pos){ // motor might be stuck
+        nSame++; 
       }
       else{
 	nSame = 0;
@@ -98,31 +100,27 @@ void loop() {
        nSame = 0;
       }
 
-
       flip = false;
       if(currentPosition == setpoint){
         nHome++;
       }
-     else if( ( (currentPosition < setpoint) && !forward) ||
-          ( (currentPosition > setpoint) && forward) ){
+      else if( ( (currentPosition < setpoint) && !forward) || // If we have overshot
+	       ( (currentPosition > setpoint) && forward) ){
   	forward = !forward;
   	nHome = 0;
   	flip = true;
-  	speed /= 1.03;
+  	speed /= 1.03; 
  	if(speed < floor_speed){
 	  speed = floor_speed;
         } 
-        wait_time = 100;
+        wait_time = wait_time_flip; // temporarily increase wait time, so the motor can finish drifting
       }
-      else{
-        nHome = 0;
-      }
-  
-      if(flip){
+      
+      if(flip){ // we have just overshot, let's stop moving temporarily
         digitalWrite(motorPin1, LOW);
         digitalWrite(motorPin2, LOW);
       }
-      else if(nHome > 499){
+      else if(nHome >= (home_limit-1)){ // we are done, stop moving the motor
         digitalWrite(motorPin1, LOW);
         digitalWrite(motorPin2, LOW);
         last_time_home = time;
@@ -137,24 +135,22 @@ void loop() {
         digitalWrite(motorPin2, HIGH);
         analogWrite(pwmPin, speed);
       } 
-    }
+    }  // end if( (time - last_time > wait_time) )
   } //end if(nHome < 500)
-  else if( (time - last_time_home) > wait_time_home){
+  else if( (time - last_time_home) > wait_time_home){ // after some amount of time, start again. Useful if you want to move the motor back and forward a bunch
     reset();
     nRun++;
     Serial.print("nRun: ");
     Serial.println(nRun);
   }
-  // Add code to change the setpoint based on desired position
-  // For example, you could read a new setpoint from Serial or a potentiometer.
 }
 
 void reset() {
   last_time_home = 0;
-  myEncoder.write(0);
+  myEncoder.write(0); // reset the current position to zero
   nHome = 0;
   speed = base_speed;
-  setpoint *= -1;
+  setpoint *= -1; // reverse the direction
 }
 
 void print() {
